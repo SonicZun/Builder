4f716916ec3c4f127e2c08877e17aa161633342c
Sonicss <17373511@buaa.edu.cn>
2019-05-03 09:18:15 +0800

    compatible with linux

--- build.py
+++ build.py
@@ -8,18 +8,26 @@ Created on Mon Apr 22 19:10:45 2019
 import os
 import re
 import sys
+import platform
 
+sysType = platform.system()
+curPath = os.getcwd()
+pathLib = os.path.join(curPath, "lib")
+pathBin = os.path.join(curPath, "bin")
+pathSrc = os.path.join(curPath, "src")
+pathPath = os.path.join(curPath, "path")
 suffix = ".+\.java"
 print("the project is being builded...\n")
-print("The current directory : " + os.getcwd() + "\n")
+print("The current directory : " + curPath + "\n")
+
 
 def clear():
-    classList = os.listdir(os.getcwd() + "\\bin\\")
+    classList = os.listdir(pathBin)
     for class_ in classList:
         if re.match(".+\.class", class_):
-            path = os.getcwd() + "\\bin\\" + class_
-            print("Delete: " + path)
-            os.remove(path)
+            pathTmp = os.path.join(pathBin, class_)
+            print("Delete: " + pathTmp)
+            os.remove(pathTmp)
 
 def findPattern(path, pattern):
     fp = open(path, "r")
@@ -43,22 +51,24 @@ def register(path, filename, suffix):
                 print(os.path.join(root,name)[len(path):] + " is registered")
     srclist.close()
     
-def findMain():
-    path = os.getcwd() + "\\src\\"
-    for root,dirs,files in os.walk(path,topdown=True):
+def findMain(system):
+    for root,dirs,files in os.walk(pathSrc,topdown=True):
         for name in files:
             if re.match(suffix, name):
                 tmp = findPattern(os.path.join(root,name), " main\(.*\)")
                 if tmp[0]:
-                    s = tmp[1][len(path):]
-                    s = s.replace("\\", ".")
+                    s = tmp[1][len(pathSrc) + 1:]
+                    print("debug: " + pathSrc)
+                    if system == "Windows":
+                    	s = s.replace("\\", ".")
+                    elif system == "Linux":
+                    	s = s.replace("/", ".")
                     return s[0 : len(s) - 5]
                     
 
 
 def getLib(system):
     jars = ""
-    pathLib = os.getcwd() + "\\lib\\"
     if system == "Windows":
         splitter = ";"
     elif system == "Linux":
@@ -71,12 +81,11 @@ def getLib(system):
 def writeMANIFEST():
     mf = open("MANIFEST.MF", "w")
     mf.write("Manifest-Version: 1.0\n")
-    mf.write("Main-Class: " + findMain() + "\n")
-    pathLib = os.getcwd() + "\\lib\\"
+    mf.write("Main-Class: " + findMain(sysType) + "\n")
     jars = "Class-Path: ."
     for root,dirs,files in os.walk(pathLib,topdown=True):
         for name in files:
-            jars = jars + " " + "lib\\" + name
+            jars = jars + " " + os.path.join("lib", name)
     jars = jars + " bin"
     mf.write(jars + "\n")
     mf.write("\n")
@@ -92,19 +101,24 @@ def rmrf(path):
        os.rmdir(path)
 
 
-rmrf(os.getcwd() + "\\path")
-rmrf(os.getcwd() + "\\bin")
-os.mkdir(os.getcwd() + "\\bin")
-os.mkdir(os.getcwd() + "\\path")
-register(os.getcwd() + "\\src\\", "path\\srclist.txt", ".+\.java")
-jars = getLib("Windows")
+rmrf(pathPath)
+rmrf(pathBin)
+os.mkdir(pathBin)
+os.mkdir(pathPath)
+register(pathSrc, os.path.join("path", "srclist.txt"), ".+\.java")
+jars = getLib(sysType)
 #print("jars : " + jars)
 writeMANIFEST()
-cmd = "@javac -encoding UTF-8 -cp " + jars + " @path\\srclist.txt -d bin" + " & " + "@jar -cvfm {}.jar MANIFEST.MF -C bin/ .".format(sys.argv[1])
+if sysType == "Windows":
+	splitter = " & "
+elif sysType == "Linux":
+	splitter = " ; "
+
+cmd = "@javac -encoding UTF-8 -cp " + jars + " @{} -d bin".format(os.path.join("path", "srclist.txt")) + splitter + "@jar -cvfm {}.jar MANIFEST.MF -C bin/ .".format(sys.argv[1])
 os.system(cmd)
-rmrf(os.getcwd() + "\\path")
-rmrf(os.getcwd() + "\\bin")
-os.remove(os.getcwd() + "\\MANIFEST.MF")
+rmrf(pathPath)
+rmrf(pathBin)
+os.remove(os.path.join(curPath, "MANIFEST.MF"))
 
 
 
